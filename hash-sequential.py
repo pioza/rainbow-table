f862fda8a92640fc86731e602ddfc3a71adb5914
pioza <pioza0@o2.pl>
2018-06-10 22:25:24 +0200

    Update hash-sequential.py

--- hash-sequential.py
+++ hash-sequential.py
@@ -1,54 +1,31 @@
-import pyDes
+from Crypto.Cipher import DES
 import re
 
-def from_bytes (data, big_endian = False):
-    if isinstance(data, str):
-        data = bytearray(data)
-    if big_endian:
-        data = reversed(data)
-    num = 0
-    for offset, byte in enumerate(data):
-        num += byte << (offset * 8)
-    return num
+def hashing(plain_text):
+    key = b'key12345' #wymyslamy klucz- musi miec 8 znakow
+    iv = b"\0\0\0\0\0\0\0\0"#tu 8 bajtow
+    cipher1 = DES.new(key, DES.MODE_CBC, iv)
+    cipher2 = DES.new(key, DES.MODE_CBC, iv)
+    #plain_text = b'cokolwiek_co_ma_24_znaki'#wymyslamy co ma byc zaszyfrowane
 
+    encrypted = cipher1.encrypt(plain_text) #zaszyforwane
+    decrypted = cipher2.decrypt(encrypted)#rozszyfrowane
 
-def hash(plain_text):
-    k = pyDes.des(b"DESCRYPT", pyDes.CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)
-    d = k.encrypt(plain_text)
-    print ("Encrypted: %r" % d)
-    print ("Decrypted: %r" % k.decrypt(d))
-    #assert k.decrypt(d) == plain_text
-    d_str=str(d)
-    print(from_bytes(d))
-    print("nr 1")
-    for e in from_bytes(d):
-        print(chr(e), end=' ')
-    print("\nnr 2")
-    for e in d:
-        print(e, end=' ')
-    print("\nnr 3")
-    for e in d:
-        print(chr(e), end=' ')
-    #with open('dictionary.txt', 'a') as dict:
-        #dict.write(plain_text+'   '+str(from_bytes(k.decrypt(d)))+'\n')
+    print(encrypted)
+    print(decrypted)
+    with open('hashes.txt', 'ab') as hashfile:
+        hashfile.write(encrypted)
+    with open('plaintextes.txt', 'a') as passes:
+        passes.write(str(decrypted)[2:-1]+'\n')
+
 
 
 def reduction(hash):
-    hash="A123BF23FFsF"
-    word1 = "".join(re.findall("[a-zA-Z]+", hash))
-    word2 = word1[:3]
+    hash = "A1s2xc3cccBF23FFseF"
+    word1 = "".join(re.findall("[a-z]+", hash)) #tylko znaki male
+    #word1 = "".join(re.findall("[a-zA-Z]+", hash)) #dla znakow duzych i malych
+    word2 = word1[:5]
     print(word1, word2)
     return word2
 
-
-hash("elo")
-'''
-data = b"pozdro"
-k = pyDes.des("aaaaaaaa", pyDes.CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)
-d = k.encrypt(data)
-print ("Encrypted: %r" % d)
-print ("Decrypted: %r" % k.decrypt(d))
-assert k.decrypt(d) == data
-'''
-
-
+hashing(b'cokolwiek_co_ma_24_znaki')
