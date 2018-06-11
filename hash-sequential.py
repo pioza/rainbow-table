55cdc4182821f506dcab9032767c7b83192b9154
pioza <pioza0@o2.pl>
2018-06-11 23:34:52 +0200

    Update hash-sequential.py

--- hash-sequential.py
+++ hash-sequential.py
@@ -5,13 +5,18 @@ import os
 def hashing():
     key = b'key12345'
     iv = b"\0\0\0\0\0\0\0\0"
-    cipher = DES.new(key, DES.MODE_CBC, iv)
     if os.path.getsize("/home/piotr/PycharmProjects/rt/plaintextes.txt") ==0:
         plain_text=b'cokolwiek_co_ma_24_znaki'
     else:
         with open('plaintextes.txt', 'rb') as file:
             plain_text=file.read()[-8:]
+    cipher = DES.new(key, DES.MODE_CBC, iv)
     encrypted = cipher.encrypt(plain_text)
+    c=0
+    while(c<5):
+        cipher = DES.new(key, DES.MODE_CBC, iv)
+        encrypted=cipher.encrypt(encrypted)
+        c+=1
     with open('hashes.txt', 'ab') as hashfile:
         hashfile.write(encrypted)
     with open('plaintextes.txt', 'ab') as passes:
