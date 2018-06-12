26fe0dee361fc2b64f1da123457804d1e4dec21d
pioza <pioza0@o2.pl>
2018-06-12 14:48:24 +0200

    Update hash-sequential.py

--- hash-sequential.py
+++ hash-sequential.py
@@ -3,20 +3,17 @@ import re
 import os
 
 def hashing():
-    key = b'key12345'
-    iv = b"\0\0\0\0\0\0\0\0"
     if os.path.getsize("/home/piotr/PycharmProjects/rt/plaintextes.txt") ==0:
         plain_text=b'cokolwiek_co_ma_24_znaki'
     else:
         with open('plaintextes.txt', 'rb') as file:
             plain_text=file.read()[-8:]
-    cipher = DES.new(key, DES.MODE_CBC, iv)
-    encrypted = cipher.encrypt(plain_text)
     c=0
-    while(c<5):
-        cipher = DES.new(key, DES.MODE_CBC, iv)
-        encrypted=cipher.encrypt(encrypted)
+    while c<5:
+        my_hash=minihash(plain_text)
+        minireduction(my_hash)
         c+=1
+    encrypted=my_hash
     with open('hashes.txt', 'ab') as hashfile:
         hashfile.write(encrypted)
     with open('plaintextes.txt', 'ab') as passes:
@@ -29,11 +26,16 @@ def reduction():
         with open('hashes.txt', 'rb') as hashfile:
             hash=hashfile.read()[-8:]
         print(hash)
-        reduced="".join(re.findall("[a-zA-Z0-9]+", str(hash)))[1:4]
-        #print(reduced)
+        reduced=minireduction(hash)
     with open('plaintextes.txt', 'ab') as passes:
         passes.write(bytes(reduced, 'utf8'))
 
+def minihash(plain_text):
+    key = b'key12345'
+    iv = b"\0\0\0\0\0\0\0\0"
+    cipher = DES.new(key, DES.MODE_CBC, iv)
+    encrypted = cipher.encrypt(plain_text)
+    return encrypted
 
-hashing()
-reduction()
+def minireduction(hash):
+    return "".join(re.findall("[a-zA-Z0-9]+", str(hash)))[1:4]
